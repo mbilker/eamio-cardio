84e5a3121604d6942bdd969acb753cade9d1f86e
Matt Bilker <me@mbilker.us>
2018-10-17 17:50:41 +0000

    test_hid: remove return when no reader is detected initially for testing device notification

--- test_hid.c
+++ test_hid.c
@@ -22,26 +22,25 @@ int WINAPI wWinMain(HINSTANCE hInstanceExe, HINSTANCE hPrevInstance, LPWSTR lpst
   hid_scan(&hid_ctx);
   printf("devices scanned\n");
 
-  if (!hid_ctx.initialized) {
-    printf("HID reader not found\n");
-    return 1;
-  }
+  if (hid_ctx.initialized) {
+    /*
+    hid_poll_value_t poll_value;
+    for (int i = 0; i < 5; i++) {
+      while ((poll_value = hid_device_poll(&hid_ctx)) == HID_POLL_CARD_NOT_READY);
+      if (poll_value == HID_POLL_ERROR) {
+        printf("Error polling reader\n");
+        return 1;
+      }
 
-  /*
-  hid_poll_value_t poll_value;
-  for (int i = 0; i < 5; i++) {
-    while ((poll_value = hid_device_poll(&hid_ctx)) == HID_POLL_CARD_NOT_READY);
-    if (poll_value == HID_POLL_ERROR) {
-      printf("Error polling reader\n");
-      return 1;
+      hid_device_read(&hid_ctx);
     }
+    printf("program exit\n");
+    */
 
-    hid_device_read(&hid_ctx);
+    hid_free(&hid_ctx);
+  } else {
+    printf("HID reader not found\n");
   }
-  printf("program exit\n");
-  */
-
-  hid_free(&hid_ctx);
 
   if (!MessagePump(hWnd)) {
     return 1;
